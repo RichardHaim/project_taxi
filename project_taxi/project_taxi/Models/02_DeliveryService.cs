42f47306a35c61024e02e6033d6c6ca522c2aeb8
Richard Haim <s50711@edu.campus02.at>
2022-11-02 16:17:57 +0100

    added data validation to delivery service model

--- project_taxi/project_taxi/Models/02_DeliveryService.cs
+++ project_taxi/project_taxi/Models/02_DeliveryService.cs
@@ -3,9 +3,8 @@ namespace project_taxi.Models
 {
     public class Fahrservice
     {
-        public Fahrservice()
-        {
-        }
+        public bool taxi_busy { get; set; } // 0 == free; 1 == busy
+        public bool event_customerDelivered { get; set; } // 0 == not delivered, 1 == delivered
+        public bool rating_clearance { get; set; } // 0 == no rating possible, 1 == customer can rate
     }
 }
-
