351aaef53943f60799e836e6b53e9ae3c5871af2
Richard Haim <s50711@edu.campus02.at>
2022-11-10 15:09:22 +0100

    taxi_busy update

--- project_taxi/project_taxi/Controllers/Overlord.cs
+++ project_taxi/project_taxi/Controllers/Overlord.cs
@@ -31,20 +31,24 @@ namespace project_taxi.Controllers
             if (DeliveryService_Controller.taxi_busy == false)
             {
                 // vergibt customer_ID, die in Liste von ZentraleService gespeichert wird
+                // speicher taxi_busy in DeliveryService_Control als true (also besetzt)
                 zentrale_ID.customer_ID = zentraleList.Count + 1;
                 zentraleList.Add(zentrale_ID);
+                DeliveryService_Controller.taxi_busy = true;
                 return Ok(zentrale_ID);
             }
+            // hier brauchen wir eine Schleife, die alle 10 Sekunden abfrägt
             return NotFound("Fahrer besetzt");
         }
 
         // hier sollte der Overlord an den 01_DeliveryService die customerID übergeben
         // gleichzeitig auch das taxi_busy vom DeliveryService auf True setzen
         // taxi_busy im Delivery Service bleibt 1 Minute lang auf True, danach automatisch auf false setzen
+
         // PUT api/<Overlord>/5
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
     }
 }
