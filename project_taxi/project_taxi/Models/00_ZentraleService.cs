f2f8733afe0287a14f2af289621e31837497be22
Richard Haim <s50711@edu.campus02.at>
2022-11-10 11:24:26 +0100

    added model zentralservice

---
+++ project_taxi/project_taxi/Models/00_ZentraleService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace project_taxi.Models
+{
+    public class _0_ZentraleService
+    {
+        public bool taxi_busy { get; set; } // 0 == free; 1 == busy
+        public string customer_ID { get; set; } // customer ID number for rating system
+    }
+}
+
