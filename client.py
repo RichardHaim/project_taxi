03bac28ff7c0ad730e2c53d2c13fd9a87019488f
Richard Haim <s50711@edu.campus02.at>
2022-11-13 20:56:01 +0100

    added client

---
+++ client.py
@@ -0,0 +1,114 @@
+import requests
+import json
+import time
+
+from contextlib import contextmanager
+import sys, os
+
+
+
+class Client():
+
+    def __init__(self):
+        self.api_url = "https://localhost:"
+        self.data = {}
+        self.headers : str={
+            'Content-type':'application/json', 
+            'Accept':'application/json'}
+        self.api_ID = "noch leer"
+
+
+    def exit_check(self, api_return):
+        if api_return.status_code == 200:
+            return
+        sys.exit(f'\n{api_return.json()}, das Programm wurde beendet\n')
+
+    def counting(self, n:int):
+        while n > 0:
+            print(n, end='\r')
+            time.sleep(1)
+            n -= 1
+        print(" ", end='\r')
+        return
+        
+
+
+    def buchung(self):
+        port = input("Bitte localhost-port eingeben: ")
+        self.api_url = self.api_url + port
+        api_connector = self.api_url + "/Buchungsanfrage" 
+        print(f'\nBuchungsanfrage gestartet\nIhr Taxi wird gebucht in...\n')
+        
+        self.counting(5)
+       
+        print(f'Ihr Buchung auf {api_connector} wird nun durchgeführt')
+        response = requests.post(api_connector, verify = False, headers = self.headers, json = self.data)
+        
+        self.exit_check(response)
+        self.api_ID = response.json()
+        print(f'Ihr Taxi wurde gebucht, Ihre Kennung ist die {self.api_ID}')
+
+        self.counting(3)
+        self.einsteigen()
+
+
+
+    def einsteigen(self):
+        api_connector = self.api_url + "/einsteigen"
+        print(f'\nIhr Taxi ist angekommen, Sie dürfen nun einsteigen\n')
+        
+        self.counting(5)
+
+        response = requests.post(api_connector, verify = False, headers = self.headers, json = self.data)
+        
+        self.exit_check(response)
+
+        print(response.json())
+        
+        self.counting(3)
+        self.aussteigen()
+
+
+    def aussteigen(self):
+        api_connector = self.api_url + "/aussteigen"
+        print(f'\nSie sind an Ihrem Ziel angekommen, Sie dürfen nun aussteigen\n')
+        
+        self.counting(5)
+
+        response = requests.put(api_connector, verify = False, headers = self.headers, json = self.data)
+        
+        self.exit_check(response)
+
+        print(response.json())
+        
+        self.counting(3)
+        #self.aussteigen()
+
+
+
+    def bewerten(self):
+        api_connector = self.api_url + "/rate_me"
+        print(f'\nSie dürfen nun die Fahrt bewerten\n')
+        stars = input("Bitte geben Sie eine Sternebewertung zwischen 1 und 5 ab: ")
+        comment = input("Hier dürfen Sie Text eingeben")
+        print("Ihre Angaben werden bearbeitet, bitte warten")
+
+        bewertung_data = {
+            "customer_ID": self.api_ID,
+            "rating_stars": stars,
+            "rating_comment": comment
+        }
+
+        self.counting(5)
+        response = requests.post(api_connector, verify = False, headers = self.headers, json = bewertung_data)
+        self.exit_check(response)
+
+        print(response.json())
+        sys.exit("Vielen Dank, dass Sie unseren Service genutzt haben & bis bald")
+
+
+
+
+if __name__ == "__main__":
+    c = Client()
+    c.buchung()
---
+++ project_taxi/project_taxi/Service/CustomerRatingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using project_taxi.Controllers;
+using project_taxi.Models;
+
+namespace project_taxi.Service
+{
+    public interface ICustomerRatingService
+    {
+        int rating_review(Kundenrating request);
+    }
+
+    public class ratingServiceValidator : ICustomerRatingService
+    {
+        public int rating_review(Kundenrating request)
+        {
+            int return_value = request.rating_stars;
+            if (request.rating_stars <0 | request.rating_stars > 5)
+            {
+                return_value = -1;
+            }
+            return return_value;
+
+        }
+    }
+}
+
